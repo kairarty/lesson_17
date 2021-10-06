465d67eec30b88550dbcfda1176d8acc177226e5
kairarty <kairarty@gmail.com>
2021-10-06 15:24:32 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,125 @@
+/*
+ * #1
+ *
+ * Создайте функцию counter(), которая должна реализовать счетчик с помощью замыкания:
+ * функция может принимать число в качестве аргумента counter(n)
+ * если число передано в функцию – счет начинается с указанного числа
+ * если нет – то счет продолжается
+ */
+
+var counter = (function () {
+    var i = 0;
+
+    return function (num) {
+        if (num === undefined) {
+            i++;
+        } else {
+            i = num;
+        }
+        return i;
+    };
+}());
+
+var counting = (function () {
+    var i = 0;
+
+    return {
+        value(num) {
+            if (num !== undefined) {
+                i = num;
+            }
+            return i;
+        },
+        increment() {
+            i++;
+        },
+        decrement() {
+            i--;
+        }
+    };
+}());
+
+
+function myPrint(a, b, res) {
+    return `${a}^${b}=${res}`;
+}
+
+function myPow(a, b, myPrint) {
+    function pow(x, n) {
+        if (n !== 1) {
+            return x * pow(x, n - 1);
+        }
+        return x;
+    }
+
+    return myPrint(a, b, pow(a, b));
+}
+
+//  console.log(myPow(3, 4, myPrint)); // 3^4=81
+
+
+var yearNow = new Date().getFullYear();
+
+function getInfoString() {
+    return this.name + ' ' + this.model + ', ' + this.engine + 'cc, year ' + this.year + ', ' + this.used;
+}
+
+var car = {
+    engine: 2000,
+    model: 'TT',
+    name: 'Audi',
+    year: 2021,
+    info: getInfoString,
+    get used() {
+        if (this.year !== yearNow) {
+            return 'used';
+        }
+        return 'new';
+    },
+    set used(value) {
+        if (value === 'new' && this.year !== yearNow) {
+            this.year = yearNow;
+        }
+    }
+}
+
+var car2 = {
+    engine: 2000,
+    model: '500',
+    name: 'Mercedes',
+    year: 2008,
+    info: getInfoString,
+    get used() {
+        if (this.year !== yearNow) {
+            return 'used';
+        }
+        return 'new';
+    },
+    set used(value) {
+        if (value === 'new' && this.year !== yearNow) {
+            this.year = yearNow;
+        }
+    }
+}
+
+
+function myMax(arr) {
+    return Math.max.apply(null, arr)
+}
+
+
+function myMul(a, b) {
+    return a * b;
+}
+
+var myDouble = myMul.bind(null, 2);
+
+let myTriple = myMul.bind(null, 3);
+
+
+
+function myUniq(arr) {
+    var set = new Set();
+    arr.forEach(val => set.add(val));
+    return set;
+}
